4a4277a31cd68b3489a71b16d92457736d9d7448
Caleb Day <calebday@Calebs-MBP.lan>
2022-07-20 17:23:29 -0400

    decent start on the About component

--- src/App.js
+++ src/App.js
@@ -6,16 +6,6 @@ import Contact from './components/Contact';
 import Project from './components/Project';
 
 function App() {
-  const [categories] = useState([
-    {
-      name: 'about',
-      description: "My name is Caleb Day, I am a web developer. In 2020, I decided to change careers from video editing to programming, but what I didn't relize was I was going to find my passion.",
-      photo: '',
-    },
-    { name: 'projects', description: 'This is a collection of my web applications I have created/helped create.' },
-    { name: 'contact', description: 'Contact me and we can move forward together.' },
-    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
-  ]);
 
   const [currentPage, setCurrentPage] = useState('About');
 
@@ -35,7 +25,10 @@ function App() {
 
   return (
     <div className="App">
-      <Nav handlePage={handlePage} />
+      <Nav 
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+        handlePage={handlePage}></Nav>
       {choosePage(currentPage)}
     </div>
   );
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
 function Contact() {
+    return(
+        <div>
+            <section class='container'>
+                <section class='section-name'>Contact Me</section>
+                <article class='contact-articles'>
+                    <p class='contact-p'>
+                        Get in touch with me so we can move forward together.
+                    </p>
+                    <p class='contact-p'>
+                        Check out my projects on <a href="https://github.com/Caleeeb" target="_blank" rel="noreferrer">GitHub</a>
+                    </p>
+                    <p class='contact-p'>
+                        Connect with me on <a href="https://www.linkedin.com/in/caleb-j-day/" target="_blank" rel="noreferrer">LinkedIn</a>
+                    </p>
+                    <p class='contact-p'>
+                        See the coding questions and answers on <a href="https://stackoverflow.com/users/19589970/caleeb" target="_blank" rel="noreferrer">Stack Overflow</a>
+                    </p>
+                </article>
+            </section>
 
+        </div>
+    )
 }
 
 export default Contact;
