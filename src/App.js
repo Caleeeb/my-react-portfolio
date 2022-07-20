a449d97570d4055be9a97081fd30f3e4491082b4
Caleb Day <calebday@Calebs-MBP.lan>
2022-07-20 11:29:42 -0400

    basic template start on nav bar component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ import Contact from './components/Contact';
 import Project from './components/Project';
 
 function App() {
+  const [categories] = useState([
+    {
+      name: 'about',
+      description: "My name is Caleb Day, I am a web developer. In 2020, I decided to change careers from video editing to programming, but what I didn't relize was I was going to find my passion.",
+      photo: '',
+    },
+    { name: 'projects', description: 'This is a collection of my web applications I have created/helped create.' },
+    { name: 'contact', description: 'Contact me and we can move forward together.' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  ]);
+
   const [currentPage, setCurrentPage] = useState('About');
 
   function choosePage(page) {
