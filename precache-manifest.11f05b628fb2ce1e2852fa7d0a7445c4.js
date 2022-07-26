37ad98eaf6a638f5b7940b93f7d36d6876e2dfd0
Caleb Day <calebday@Calebs-MBP.lan>
2022-07-26 10:46:37 -0400

    Updates

---
+++ precache-manifest.11f05b628fb2ce1e2852fa7d0a7445c4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "00450c473d567bd0deb6",
+    "url": "/my-react-portfolio/static/css/main.716e881c.chunk.css"
+  },
+  {
+    "revision": "00450c473d567bd0deb6",
+    "url": "/my-react-portfolio/static/js/main.00450c47.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/my-react-portfolio/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "b2a974f158ad192369a3",
+    "url": "/my-react-portfolio/static/js/2.b2a974f1.chunk.js"
+  },
+  {
+    "revision": "06caf0e736d9c3c28790",
+    "url": "/my-react-portfolio/static/js/runtime~main.06caf0e7.js"
+  },
+  {
+    "revision": "6a80a7f13c3bafb793c3476b74d54f73",
+    "url": "/my-react-portfolio/static/media/CalebDay1.6a80a7f1.jpeg"
+  },
+  {
+    "revision": "6a7c4d6f921c7647827c44ab557fb0bd",
+    "url": "/my-react-portfolio/static/media/drink-app.6a7c4d6f.jpeg"
+  },
+  {
+    "revision": "c1d1ed12edbf1ba2da23c91ff2b54e19",
+    "url": "/my-react-portfolio/static/media/gifcast.c1d1ed12.png"
+  },
+  {
+    "revision": "ec13c7d5c81f1f0e58944b267fa20d5e",
+    "url": "/my-react-portfolio/static/media/glassfiller.ec13c7d5.png"
+  },
+  {
+    "revision": "0baae4deee2b37acdc9fad0b5a9133de",
+    "url": "/my-react-portfolio/static/media/note-taker.0baae4de.png"
+  },
+  {
+    "revision": "f0d0865b3ab368ef43b2ecb1e9304d85",
+    "url": "/my-react-portfolio/static/media/portfolioSS.f0d0865b.png"
+  },
+  {
+    "revision": "5aa8e7b8929ef4587b93f51abbf4c556",
+    "url": "/my-react-portfolio/static/media/run-buddy.5aa8e7b8.jpeg"
+  },
+  {
+    "revision": "c29ab862190699e13d40068e9a1a85bd",
+    "url": "/my-react-portfolio/static/media/work-day-scheduler.c29ab862.png"
+  },
+  {
+    "revision": "e0e63104f51732db389e8aa195ddc020",
+    "url": "/my-react-portfolio/index.html"
+  }
+];
